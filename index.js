b8442b1b082a232237986cd288439b1b041bcee8
Sarah Jones <sarahrjones8@gmail.com>
2023-03-18 12:18:54 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Sarah Jones';
+
+const welcomeMessage = `Welcome to Flatbook, ' + ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
